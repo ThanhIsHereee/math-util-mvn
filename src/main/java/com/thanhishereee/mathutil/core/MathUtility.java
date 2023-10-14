b22153c2bdf691ca37147e519b26863cac19d6cf
ThanhIsHereee <nquangthanh88@gmail.com>
2023-10-14 16:52:23 +0700

    update getf() to use recursion, regression test and green4ever

--- src/main/java/com/thanhishereee/mathutil/core/MathUtility.java
+++ src/main/java/com/thanhishereee/mathutil/core/MathUtility.java
@@ -18,15 +18,32 @@ public class MathUtility {
     //n < 0 | n > 20 ko tính do 21! tràn bộ nhớ kiểu long
     //ko tính giai thừa âm
     
+//    public static long getFactorial(int n){
+//        long result = 1;
+//        
+//        if(n < 0 || n > 20)
+//            throw new IllegalArgumentException("Invalid n. n have to be among 0 to 20.");
+//        if(n == 0 || n == 1) 
+//            return 1;
+//        for (int i = 2; i <= n; i++) 
+//            result *= i;
+//        return result;
+//    }
+    //Update hàm tính giai thừa bằng đệ quy
+    //n! = 1*2*3*4...*n
+    //3! = 1*2*3
+    //3! = 2! * 3
+    //2! = 1! * 2
+    //1! = 1 là dừng
+    // => n! = n * (n - 1)!
+    // đệ quy - recursion
+    // -> gọi lại chính mình với 1 quy mô nhỏ hơn
+    
     public static long getFactorial(int n){
-        long result = 10;
-        
         if(n < 0 || n > 20)
             throw new IllegalArgumentException("Invalid n. n have to be among 0 to 20.");
         if(n == 0 || n == 1) 
             return 1;
-        for (int i = 2; i <= n; i++) 
-            result *= i;
-        return result;
+        return n * getFactorial(n - 1);
     }
 }
